a062962f948c6394676bbf635c6e4e5f121e0931
ehz0ah <haozhe4547@gmail.com>
2025-05-11 17:47:23 +0800

    Add logo & scrollbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -10,6 +10,8 @@ import {
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 
+import SearchBar from "@/components/SearchBar";
+
 
 export default function Index() {
   return (
@@ -19,6 +21,24 @@ export default function Index() {
         className="absolute w-full z-0"
         resizeMode="cover"
       />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => {
+                router.push("/search");
+              }}
+              placeholder="Search for a movie"
+            />
+        </View>
+      </ScrollView>
+
     </View>
   );
 }
