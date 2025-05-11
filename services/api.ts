3f1d55b3b03530ab8b3922deda93589073121a91
ehz0ah <haozhe4547@gmail.com>
2025-05-11 18:32:16 +0800

    Setup client side API wrapper

---
+++ services/api.ts
@@ -0,0 +1,40 @@
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+
+/**
+ * Fetches a list of movies from the TMDB API based on a search query.
+ * If a query is provided, it searches for movies matching the query.
+ * If no query is provided, it fetches the most popular movies.
+ *
+ * @param {Object} params - The parameters for fetching movies.
+ * @param {string} params.query - The search query for movies.
+ * @returns {Promise<Movie[]>} - A promise that resolves to an array of Movie objects.
+ * @throws {Error} - Throws an error if the fetch operation fails.
+ */
+
+export const fetchMovies = async ({ query }: { query: string }): Promise<Movie[]> => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch movies: ${response.statusText}`);
+  }
+
+  const data = await response.json();
+  return data.results;
+};
+
+
